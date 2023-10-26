8b0f9932c72dfe4c112af42c0c006dbaedba0561
Neet kaur <neetkhinda567@gmail.com>
2023-10-26 15:20:00 -0400

    tcommit

--- Activity1/src/Q8.java
+++ Activity1/src/Q8.java
@@ -4,19 +4,18 @@ public class Q8 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	        Scanner scanner = new Scanner(System.in);
-
+	        Scanner kk = new Scanner(System.in);
 	        System.out.print("Enter a number: ");
-	        double number = scanner.nextDouble();
-
-	        if (number < 0) {
-	            System.out.println("Square root is undefined for negative numbers.");
+	        double db = kk.nextDouble();
+	        
+	        if(db<0) {
+	        	System.out.print("can't do the square root because the number is negative ");	
 	        } else {
-	            double squareRoot = Math.sqrt(number);
-	            System.out.println("Square root of " + number + " is " + squareRoot);
+	        		double squareRoot = Math.sqrt(db);
+	            System.out.println("Square root of " + db + " is " + squareRoot);
 	        }
 
-	        scanner.close();
+	        kk.close();
 	    
 	}
 
